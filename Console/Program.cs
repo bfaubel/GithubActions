83e90157b97b6210ad0cee54dfd323d6e1841aee
Ben Faubel <ben.faubel@protonmail.com>
2022-04-03 18:42:53 -0500

    ImplementPowFaubel

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace GithubActionsLab
+namespace TravisCI
 {
     public class Program
     {
@@ -84,7 +84,7 @@ namespace GithubActionsLab
         // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            throw new NotImplementedException();
+            return Math.Pow(double.Parse(x), double.Parse(y));
         }
     }
 
